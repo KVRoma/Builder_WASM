99f277b14c7438ecf8c0703d8534a696d7471209
Roman Kuchinik <kvroma83@gmail.com>
2022-05-22 23:35:56 +0300

    Added "Sign Up" page

--- Builder_WASM/Server/Controllers/UserRegisteredsController.cs
+++ Builder_WASM/Server/Controllers/UserRegisteredsController.cs
@@ -9,10 +9,11 @@ using Builder_WASM.Server.Data;
 using Builder_WASM.Shared.Entities;
 using Builder_WASM.Server.Services;
 using Microsoft.AspNetCore.Authorization;
+using Builder_WASM.Shared.Models;
 
 namespace Builder_WASM.Server.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     [Route("api/[controller]")]
     [ApiController]
     public class UserRegisteredsController : ControllerBase
@@ -28,22 +29,22 @@ namespace Builder_WASM.Server.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<UserRegistered>>> GetUserRegistereds()
         {
-          if (_context.UserRegisteredRepository == null)
-          {
-              return NotFound();
-          }            
-          var result = await _context.UserRegisteredRepository.GetAsync(x => x.Role != "Admin");
-          return Ok(result);
+            if (_context.UserRegisteredRepository == null)
+            {
+                return NotFound();
+            }
+            var result = await _context.UserRegisteredRepository.GetAsync(x => x.Role != "Admin");
+            return Ok(result);
         }
 
         // GET: api/UserRegistereds/5
         [HttpGet("{id}")]
         public async Task<ActionResult<UserRegistered>> GetUserRegistered(int id)
         {
-          if (_context.UserRegisteredRepository == null)
-          {
-              return NotFound();
-          }
+            if (_context.UserRegisteredRepository == null)
+            {
+                return NotFound();
+            }
             var userRegistered = await _context.UserRegisteredRepository.GetByIdAsync(id);
 
             if (userRegistered == null)
@@ -63,7 +64,7 @@ namespace Builder_WASM.Server.Controllers
             {
                 return BadRequest();
             }
-            var user = await _context.UserRegisteredRepository.GetByIdAsync(id);                     
+            var user = await _context.UserRegisteredRepository.GetByIdAsync(id);
             user.CopyWithoutPassword(userRegistered);
 
             _context.UserRegisteredRepository.Update(user);
@@ -91,17 +92,32 @@ namespace Builder_WASM.Server.Controllers
 
         // POST: api/UserRegistereds
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [AllowAnonymous]
         [HttpPost]
-        public async Task<ActionResult<UserRegistered>> PostUserRegistered(UserRegistered userRegistered)
+        public async Task<ActionResult<UserRegistered>> PostUserRegistered(AuthenticateRequestSignup response)
         {
-          if (_context.UserRegisteredRepository == null)
-          {
-              return Problem("Entity set 'UserRegistereds'  is null.");
-          }
-            _context.UserRegisteredRepository.Insert(userRegistered);
+            if (_context.UserRegisteredRepository == null)
+            {
+                return Problem("Entity set 'UserRegistereds'  is null.");
+            }
+
+            var user = (await _context.UserRegisteredRepository.GetAsync(x=>x.Name == response.Username)).FirstOrDefault();
+            if (user != null)
+            {
+                return BadRequest(new {message = "This username already exists!" } );
+            }
+
+            user = new UserRegistered() 
+            {
+                Name = response.Username,
+                Password = response.Password,
+                Role = "User"
+            };           
+
+            _context.UserRegisteredRepository.Insert(user);
             await _context.SaveAsync();
 
-            return CreatedAtAction("GetUserRegistered", new { id = userRegistered.Id }, userRegistered);
+            return CreatedAtAction("GetUserRegistered", new { id = user.Id }, user);
         }
 
         // DELETE: api/UserRegistereds/5
@@ -128,6 +144,6 @@ namespace Builder_WASM.Server.Controllers
         {
             return _context.UserRegisteredRepository.Exist(id);
         }
-                
+
     }
 }
---
+++ Builder_WASM/Shared/Models/AuthenticateRequestSignup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_WASM.Shared.Models
+{
+    public class AuthenticateRequestSignup
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        
+        public string MessageNewUser { get; set; } = string.Empty;
+
+    }
+}
